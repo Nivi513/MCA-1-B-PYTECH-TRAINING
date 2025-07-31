4c592e406c5c6bd338c5c352ffe84ef08f5587a4
Nivi513 <nivethab513@gmail.com>
2025-07-31 17:50:39 +0530

    learning python

---
+++ simple calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum)
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum)
