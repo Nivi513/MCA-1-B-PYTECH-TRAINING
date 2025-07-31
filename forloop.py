8ef627aa3399ec4ca00ed2b67d17691b4767a22b
Nivi513 <nivethab513@gmail.com>
2025-07-31 18:36:40 +0530

    learning python

---
+++ forloop.py
@@ -0,0 +1,14 @@
+#for i in range(1,6):
+    #print (i)
+    
+    
+    
+#for i in range(2,11,2): 
+    #print(i)
+    
+    
+#for i in range(5,0,-1):
+        #print(i)
+        
+for char in "python":
+    print(char)
