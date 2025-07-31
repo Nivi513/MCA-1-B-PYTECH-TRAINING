c250dfe5cff76f0010489dbe987cf60bc0b2372e
Nivi513 <nivethab513@gmail.com>
2025-07-31 18:48:42 +0530

    learning python

---
+++ list(user input).py
@@ -0,0 +1,6 @@
+fruits=["apple","banana","mango","grapes"]
+favorite=input("enter your favorite fruit:")
+if favorite.lower() in fruits:
+    print("yes,it is in list")
+else:
+    print("no.it is not in list")
