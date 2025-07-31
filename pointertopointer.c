bcb1142bd62d997c80f91ee22af9599fbd9886b7
Nivi513 <nivethab513@gmail.com>
2025-07-31 20:38:25 +0530

    learning c programming

---
+++ pointertopointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a=5;
+	int*p=&a;
+	int**pp=&p;
+
+	printf("a: %d\n", a);       
+        printf("*p: %d\n", *p);     
+        printf("**pp: %d\n", **pp);
+	return 0;
+}
